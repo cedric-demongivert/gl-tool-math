ca70596727c143b389539dc3a51c0e9975d5544a
Cédric DEMONGIVERT <cedric.demongivert@gmail.com>
2018-11-18 14:16:03 -0500

    ci

--- generate.sh
+++ generate.sh
@@ -2,11 +2,11 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-python $DIR/generation/generate-vectors.py
-python $DIR/generation/generate-vectors-tests.py
-python $DIR/generation/generate-vectors-wrappers.py
-python $DIR/generation/generate-vectors-wrappers-tests.py
-python $DIR/generation/generate-matrices.py
-python $DIR/generation/generate-matrices-tests.py
-python $DIR/generation/generate-matrices-wrappers.py
-python $DIR/generation/generate-matrices-wrappers-tests.py
+python3 $DIR/generation/generate-vectors.py
+python3 $DIR/generation/generate-vectors-tests.py
+python3 $DIR/generation/generate-vectors-wrappers.py
+python3 $DIR/generation/generate-vectors-wrappers-tests.py
+python3 $DIR/generation/generate-matrices.py
+python3 $DIR/generation/generate-matrices-tests.py
+python3 $DIR/generation/generate-matrices-wrappers.py
+python3 $DIR/generation/generate-matrices-wrappers-tests.py
