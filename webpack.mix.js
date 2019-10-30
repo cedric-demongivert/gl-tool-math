f29864b6a337c1da64a6212ce7745815d3fa4fe5
Cédric DEMONGIVERT <demc9001@usherbrooke.ca>
2019-10-30 17:09:59 -0400

    ts config

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix')
+const TypedocWebpackPlugin = require('typedoc-webpack-plugin');
 const pckg = require('./package.json')
 
 const externals = []
@@ -7,7 +8,7 @@ for (const name in pckg.dependencies) {
   externals.push(new RegExp(`^${name}(\\/.+)?$`))
 }
 
-mix.js('src/index.js', 'dist')
+mix.ts('src/ts/index.ts', 'dist')
    .copy('LICENSE.md', 'dist')
    .copy('package.json', 'dist')
    .copy('README.md', 'dist')
@@ -15,8 +16,15 @@ mix.js('src/index.js', 'dist')
    .disableNotifications()
    .webpackConfig({
      'externals': externals,
+     'optimization': {
+    		'minimize': false
+     },
      'output': {
        'library': pckg.name,
-       'libraryTarget': 'umd'
-     }
+       'libraryTarget': 'umd',
+       'globalObject': 'this' // webpack bug
+     },
+     'plugins': [
+       new TypedocWebpackPlugin({ 'target': 'es6', 'mode': 'file' }, './src/ts')
+     ]
    })
