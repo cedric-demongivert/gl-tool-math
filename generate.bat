6cc3f8a393ccd793d3c989a8c500f317c3247a6d
Cédric <cedric.demongivert@gmail.com>
2020-04-17 18:00:46 -0400

    typescript types

--- generate.bat
+++ generate.bat
@@ -1,10 +1,10 @@
 @echo off
 
-python %~dp0/generation/generate-vectors.py
-python %~dp0/generation/generate-vectors-tests.py
-python %~dp0/generation/generate-vectors-wrappers.py
-python %~dp0/generation/generate-vectors-wrappers-tests.py
-python %~dp0/generation/generate-matrices.py
-python %~dp0/generation/generate-matrices-tests.py
-python %~dp0/generation/generate-matrices-wrappers.py
-python %~dp0/generation/generate-matrices-wrappers-tests.py
+python3 %~dp0/generation/generate-vectors.py
+python3 %~dp0/generation/generate-vectors-tests.py
+python3 %~dp0/generation/generate-vectors-wrappers.py
+python3 %~dp0/generation/generate-vectors-wrappers-tests.py
+python3 %~dp0/generation/generate-matrices.py
+python3 %~dp0/generation/generate-matrices-tests.py
+python3 %~dp0/generation/generate-matrices-wrappers.py
+python3 %~dp0/generation/generate-matrices-wrappers-tests.py
